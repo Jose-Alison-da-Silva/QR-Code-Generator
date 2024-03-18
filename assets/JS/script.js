2daec9505c45a34517fb9dbb3cee55cfc63f685a
Alison Silva <alisonsilva0999@gmail.com>
2024-03-18 15:12:39 -0300

    QR Code Generator

---
+++ assets/JS/script.js
@@ -0,0 +1,17 @@
+const imgBox = document.querySelector("#imgBox");
+const qrImg = document.querySelector("#qrImg");
+const qrText = document.querySelector("#qrText");
+
+function generateQR() {
+    if (qrText.value.length > 0) {
+        qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+        imgBox.classList.add("show-img");
+    }else{
+        qrText.classList.add("error");
+        setTimeout(() => {
+        qrText.classList.remove("error");
+        }, 1000);
+
+    }
+    
+}
